dbe04744a817c65b728d6a28d249f740f7463ea9
Lewis Evans <60709927+awesomelewis2007@users.noreply.github.com>
2023-04-18 10:09:10 +0100

    Update quickpull.sh

--- src/quickpull.sh
+++ src/quickpull.sh
@@ -1,6 +1,7 @@
 # QuickPull
 # Pulls git repositories in the current directory
 # Github://www.github.com/awesomeslewis/quickpull
+# By: Lewis Evans
 
 echo "Pull all repositories in the current directory? (y/n)"
 read answer
